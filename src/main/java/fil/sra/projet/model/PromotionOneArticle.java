a8c18e61787452cd8bd9084ece10f4e9b444ef09
estebancollard <estebancollard@hotmail.fr>
2021-10-24 12:57:14 +0200

    Fix : ajout promotion 1 article OK

--- src/main/java/fil/sra/projet/model/PromotionOneArticle.java
+++ src/main/java/fil/sra/projet/model/PromotionOneArticle.java
@@ -1,28 +1,20 @@
 package fil.sra.projet.model;
 
-public class PromotionOneArticle extends Promotion{
-    String idArticle;
+import javax.persistence.*;
+
+//@Inheritance(strategy = InheritanceType.SINGLE_TABLE) // ajouter param
+@Entity
+public class PromotionOneArticle{// extends Promotion{
     int percentage;
     String reference;
-
 
 	public String getReference() {
 		return reference;
 	}
 
-
-
 	public void setReference(String reference) {
 		this.reference = reference;
 	}
-
-	public String getIdArticle() {
-        return idArticle;
-    }
-
-    public void setIdArticle(String idArticle) {
-        this.idArticle = idArticle;
-    }
 
     public int getPercentage() {
         return percentage;
@@ -30,5 +22,66 @@ public class PromotionOneArticle extends Promotion{
 
     public void setPercentage(int percentage) {
         this.percentage = percentage;
+    }
+
+
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long idPromotion;
+    private String dateStart = null;
+    private String dateEnd = null;
+    private String typeReduc = null;
+    private int value = 0;
+
+
+
+
+
+
+
+
+
+
+
+    public String getDateStart() {
+        return dateStart;
+    }
+
+    public String getDateEnd() {
+        return dateEnd;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setDateStart(String dateStart) {
+        this.dateStart = dateStart;
+    }
+
+    public void setDateEnd(String dateEnd) {
+        this.dateEnd = dateEnd;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public long getIdPromotion() {
+        return idPromotion;
+    }
+
+    public String getTypeReduc() {
+        return typeReduc;
+    }
+
+    public void setTypeReduc(String typeReduc) {
+        this.typeReduc = typeReduc;
+    }
+
+    public void setIdPromotion(long idPromotion) {
+        this.idPromotion = idPromotion;
     }
 }
--- src/main/java/fil/sra/projet/web/PromotionController.java
+++ src/main/java/fil/sra/projet/web/PromotionController.java
@@ -3,32 +3,16 @@ package fil.sra.projet.web;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintWriter;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import fil.sra.projet.dao.*;
+import fil.sra.projet.model.*;
+import fil.sra.projet.repository.PromotionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
-import fil.sra.projet.dao.ArticleDao;
-import fil.sra.projet.dao.CartDao;
-import fil.sra.projet.dao.DataException;
-import fil.sra.projet.dao.OrderDao;
-import fil.sra.projet.model.Article;
-import fil.sra.projet.model.Cart;
-import fil.sra.projet.model.Order;
-import fil.sra.projet.model.PromotionOneArticle;
 import fil.sra.projet.repository.OrderRepository;
-import fil.sra.projet.web.dto.CartEntry;
 import fil.sra.projet.web.dto.SimpleResponse;
 import fil.sra.projet.web.dto.SimpleResponse.Status;
 
@@ -40,9 +24,12 @@ public class PromotionController {
 	CartDao daoCart;
 	@Autowired
 	ArticleDao daoArt;
+	@Autowired
+	PromotionRepository promotionOneArticleRepository;
 
 	@Autowired
 	OrderRepository orderRepo;
+
 
 	@ExceptionHandler(DataException.class)
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
@@ -58,9 +45,38 @@ public class PromotionController {
 	@RequestMapping(path = "/forms.html", produces = "text/html")
 	public String getForms(Model model) throws DataException {
 		model.addAttribute("promotionOneArticle", new PromotionOneArticle());
-		
 		return "admin";
+	}
 
+	@RequestMapping(value = "/promotionArticle.html", method = RequestMethod.POST, produces = "text/html")
+	public SimpleResponse addOnePromotion(@ModelAttribute("promotionOneArticle") PromotionOneArticle promotionOneArticle) {
+
+
+		SimpleResponse res = new SimpleResponse();
+
+		try {
+			if(promotionOneArticle.getValue()>0){
+				res.message = "invalid value";
+				res.status = Status.ERROR;
+			} else if (promotionOneArticleRepository.findByIdPromotion(promotionOneArticle.getIdPromotion())!=null) {
+				res.message = "id promotion already exists";
+				res.status = Status.ERROR;
+			} else if (daoArt.find(promotionOneArticle.getReference()) == null) {
+				res.message = "product doesn't exist";
+				res.status = Status.ERROR;
+				System.out.println("id = "+promotionOneArticle.getReference());
+			} else {
+
+				promotionOneArticleRepository.save(promotionOneArticle);
+
+				res.status = Status.OK;
+			}
+
+		} catch (DataException e) {
+			res.message = "error dataException";
+			res.status = Status.ERROR;
+		}
+		return res;
 	}
 
 }
